8cd989ad5ca8d613e7d53a15289cd5be8e4b49db
abilanravi <97493499+abilanravi@users.noreply.github.com>
2022-01-31 22:17:55 -0500

    Add files via upload

---
+++ Digital Clock.py
@@ -0,0 +1,22 @@
+import time
+import sys
+from tkinter import Tk
+from tkinter import Label
+
+#Tk for the screen and Label to use words
+
+master_screen = Tk()
+master_screen.title("Abilan's Digital Clock")
+
+def get_time():
+    timevari = time.strftime("%H:%M:%S %p")
+    clock.config(text=timevari)
+    clock.after(100,get_time)
+
+Label(master_screen, font=("Times New Roman", 35), text="Abilan's Digital Clock", bg="yellow", fg="black").pack()
+clock = Label(master_screen, font=("Impact",90), bg="yellow", fg="black")
+clock.pack()
+
+get_time()
+
+master_screen.mainloop()
